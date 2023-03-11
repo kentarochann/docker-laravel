18d5698a28c67db58a2c65b97f0d99fbd8b8885d
kentarochann <kentaro4413@gmail.com>
2023-03-11 15:54:59 +0900

    clean: 不要なコメントアウトを削除

--- src/resources/views/User/show.blade.php
+++ src/resources/views/User/show.blade.php
@@ -6,9 +6,7 @@
 
 <h3>user show</h3>
 <div>{{ $user->name }}</div>
-{{-- {{ Form::open([route('users.destroy', $user->id), 'method' => 'post']) }} --}}
 {{ Form::open(['route' =>['users.destroy', $user->id], 'method' => 'delete']) }}
-{{-- <button type="submit">このuserを削除</button> --}}
     {{ Form::submit('このuserを削除') }}
 {{ Form::close()}}
 
